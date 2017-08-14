b23d950845779f997df7c9bea56c6faed7b84ce4
Kevin Sweeney <kevin_sweeney@apple.com>
2017-08-14 19:42:09 +0100

    Run flake8 over Python code

--- Python/decorator_generator_syntax.py
+++ Python/decorator_generator_syntax.py
@@ -1,63 +1,83 @@
 #!/usr/bin/env python3
-import asyncio
-import random
+from asyncio import (
+    as_completed,
+    coroutine,
+    ensure_future,
+    get_event_loop,
+    sleep,
+)
+from random import random
 
 
-class Request(object): pass
+class Request(object):
+    pass
+
+
 class Response(object):
     def __init__(self, id_=0):
         self.id = id_
 
     def __repr__(self):
         return "Response(id_=%d)" % self.id
-    
-class ServiceError(Exception): pass
-class NoDownstreamService(ServiceError): pass
-class AllDowntstreamServicesFailed(ServiceError): pass
+
+
+class ServiceError(Exception):
+    pass
+
+
+class NoDownstreamService(ServiceError):
+    pass
+
+
+class AllDowntstreamServicesFailed(ServiceError):
+    pass
 
 
 class DelayService(object):
-    def __init__(self, id_):
+    def __init__(self, id_, delay_secs):
         self.id = id_
-        self.delay_secs = random.random()
+        self.delay_secs = delay_secs
 
-    @asyncio.coroutine
+    @coroutine
     def service(self, request):
-        yield from asyncio.sleep(self.delay_secs)
+        yield from sleep(self.delay_secs)
         return Response(id_=self.id)
-        
+
 
 class FirstResponseService(object):
     def __init__(self, downstream_services):
         self._downstream_services = downstream_services
 
-
-    @asyncio.coroutine
+    @coroutine
     def service(self, request):
         if not self._downstream_services:
             raise NoDownstreamService()
 
-        response_futures = [s.service(request) for s in self._downstream_services]
-        for completed_future in asyncio.as_completed(response_futures):
+        response_futures = [
+            ensure_future(s.service(request))
+            for s in self._downstream_services
+        ]
+        for completed_future in as_completed(response_futures):
             try:
                 return (yield from completed_future)
             except Exception as e:
                 print(e)
                 continue
         raise AllDowntstreamServicesFailed()
-        
 
-@asyncio.coroutine
+
+@coroutine
 def test():
-    frs = FirstResponseService(downstream_services=[DelayService(i) for i in range(50)])
+    downstream_services = [DelayService(i, random()) for i in range(50)]
+    frs = FirstResponseService(downstream_services)
     result = yield from frs.service(Request())
     print(result)
 
 
 def main():
-    loop = asyncio.get_event_loop()
+    loop = get_event_loop()
     loop.run_until_complete(test())
-    #loop.close()
+    # loop.close()
 
 
 if __name__ == '__main__':
--- Python/test_first_response_service.py
+++ Python/test_first_response_service.py
@@ -1,6 +1,5 @@
-import asyncio
-import time
-import unittest
+from asyncio import get_event_loop
+from unittest import TestCase
 
 import async_await_syntax
 import decorator_generator_syntax
@@ -8,19 +7,20 @@ import decorator_generator_syntax
 
 EPSILON_SECS = 0.1
 
-def do_test_excecution_time(test, coro):
-    loop = asyncio.get_event_loop()
-    start_time = loop.time()
-    result = loop.run_until_complete(coro())
-    elapsed_time_secs = abs(loop.time() - start_time)
-    test.assertTrue(elapsed_time_secs <= 1 + EPSILON_SECS)
-    
 
-class AsyncAwaitSyntaxTest(unittest.TestCase):
+def do_test_excecution_time(test, coro):
+    loop = get_event_loop()
+    start_time = loop.time()
+    loop.run_until_complete(coro())
+    elapsed_time_secs = abs(loop.time() - start_time)
+    test.assertAlmostEqual(elapsed_time_secs, 0.5, delta=0.5 + EPSILON_SECS)
+
+
+class AsyncAwaitSyntaxTest(TestCase):
     def test_execution_time(self):
         do_test_excecution_time(self, async_await_syntax.test)
 
 
-class DecoratorGeneratorSyntaxTest(unittest.TestCase):
+class DecoratorGeneratorSyntaxTest(TestCase):
     def test_execution_time(self):
         do_test_excecution_time(self, decorator_generator_syntax.test)
